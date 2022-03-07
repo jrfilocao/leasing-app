0e1e5adf460c99f38f3f11f12825767c179e6c8b
jrfilocao <bla@email.com>
2022-03-07 23:07:29 +0100

    Add getter to CustomerNotFoundException

--- src/main/java/com/fakegmbh/leasingapp/customer/exception/CustomerNotFoundException.java
+++ src/main/java/com/fakegmbh/leasingapp/customer/exception/CustomerNotFoundException.java
@@ -1,9 +1,11 @@
 package com.fakegmbh.leasingapp.customer.exception;
 
+import lombok.Getter;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
 @ResponseStatus(HttpStatus.NOT_FOUND)
+@Getter
 public class CustomerNotFoundException extends RuntimeException {
 
     private final Long customerId;
