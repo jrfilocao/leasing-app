de65dd7ab5c001073b06008f248eef924f95135c
jrfilocao <bla@email.com>
2022-03-07 23:07:11 +0100

    Change CustomerMapperTest in order to actual test the mapper functionality without mock

--- src/test/java/com/fakegmbh/leasingapp/customer/model/CustomerMapperTest.java
+++ src/test/java/com/fakegmbh/leasingapp/customer/model/CustomerMapperTest.java
@@ -3,8 +3,6 @@ package com.fakegmbh.leasingapp.customer.model;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.InjectMocks;
-import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.modelmapper.ModelMapper;
 
@@ -14,7 +12,6 @@ import java.time.LocalDate;
 import java.time.Month;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
 public class CustomerMapperTest {
@@ -25,17 +22,18 @@ public class CustomerMapperTest {
     private static final int BIRTH_YEAR = 1985;
     private static final int BIRTHDAY = 30;
 
-    @Mock
     private ModelMapper modelMapper;
 
-    @InjectMocks
     private CustomerMapper customerMapper;
 
     private CustomerDto customerDto;
     private CustomerEntity customer;
+    private CustomerEntity customerWithoutTimestamps;
 
     @BeforeEach
     public void setUp() {
+        modelMapper = new ModelMapper();
+        customerMapper = new CustomerMapper(modelMapper);
         final LocalDate birthdate = LocalDate.of(BIRTH_YEAR, Month.APRIL, BIRTHDAY);
         customerDto = CustomerDto.builder()
                                  .customerId(ID)
@@ -52,18 +50,23 @@ public class CustomerMapperTest {
                                  .updatedAt(Timestamp.from(Instant.now()))
                                  .createdAt(Timestamp.from(Instant.now()))
                                  .build();
+
+        customerWithoutTimestamps = CustomerEntity.builder()
+                                 .customerId(ID)
+                                 .firstName(FIRST_NAME)
+                                 .lastName(LAST_NAME)
+                                 .birthdate(birthdate)
+                                 .build();
     }
 
     @Test
     public void testMapToCustomerDto() {
-        when(modelMapper.map(customer, CustomerDto.class)).thenReturn(customerDto);
         assertThat(customerMapper.mapTo(customer)).isEqualTo(customerDto);
     }
 
     @Test
     public void testMapToCustomer() {
-        when(modelMapper.map(customerDto, CustomerEntity.class)).thenReturn(customer);
-        assertThat(customerMapper.mapTo(customerDto)).isEqualTo(customer);
+        assertThat(customerMapper.mapTo(customerDto)).isEqualTo(customerWithoutTimestamps);
     }
 }
 
