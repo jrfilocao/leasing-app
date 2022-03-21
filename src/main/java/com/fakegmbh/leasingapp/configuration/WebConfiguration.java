9f40bf9c7742f6e558dba73bebf096f65c7ae487
jrfilocao <bla@email.com>
2022-03-21 16:20:02 +0100

    Add dummy angular frontend as a submodule serving as basis for the leasing app frontend
    This angular example app came from an angular tutorial

---
+++ src/main/java/com/fakegmbh/leasingapp/configuration/WebConfiguration.java
@@ -0,0 +1,25 @@
+package com.fakegmbh.leasingapp.configuration;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfiguration implements WebMvcConfigurer  {
+
+    private static final String[] CLASSPATH_RESOURCE_LOCATIONS = {
+            "classpath:/META-INF/resources/", "classpath:/resources/",
+            "classpath:/static/", "classpath:/public/" };
+
+    @Override
+    public void addViewControllers(final ViewControllerRegistry registry) {
+        registry.addRedirectViewController("/", "index.html");
+    }
+
+    @Override
+    public void addResourceHandlers(final ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("**")
+                .addResourceLocations(CLASSPATH_RESOURCE_LOCATIONS);
+    }
+}
