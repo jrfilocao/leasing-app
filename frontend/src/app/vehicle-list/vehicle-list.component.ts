bf5ee850bb33e3e9e76bb1cd5860617ea4530c55
jrfilocao <bla@email.com>
2022-03-24 19:34:05 +0100

    Add table into vehicle's list with ngx easy table

--- frontend/src/app/vehicle-list/vehicle-list.component.ts
+++ frontend/src/app/vehicle-list/vehicle-list.component.ts
@@ -1,7 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit, TemplateRef, ViewChild} from '@angular/core';
 
 import { Vehicle } from '../api/models/vehicle'
 import { VehicleService } from '../api/services/vehicle.service'
+import {Columns, Config, DefaultConfig} from "ngx-easy-table";
 
 @Component({
   selector: 'app-vehicle-list',
@@ -10,11 +11,27 @@ import { VehicleService } from '../api/services/vehicle.service'
 })
 export class VehicleListComponent implements OnInit {
 
+  @ViewChild('actionTpl', { static: true }) actionTpl: TemplateRef<any>;
+
   vehicles: Vehicle[] = [];
+  configuration: Config;
+  columns: Columns[];
 
   constructor(private vehicleService: VehicleService) { }
 
   ngOnInit(): void {
+    this.configuration = { ...DefaultConfig };
+    this.configuration.paginationEnabled = false;
+    this.configuration.resizeColumn = true;
+    this.configuration.fixedColumnWidth = false;
+    this.columns = [
+      { key: 'brand', title: 'Brand' },
+      { key: 'model', title: 'Model' },
+      { key: 'modelYear', title: 'Model Year' },
+      { key: 'vin', title: 'VIN' },
+      { key: 'price', title: 'Price' },
+      { key: 'details', title: 'Details', cellTemplate: this.actionTpl },
+    ];
     this.getVehicles();
   }
 
